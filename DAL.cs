0d07d8fd8ce8edf60151fba506aaa4fa93904703
DESKTOP-ND87ERP\Mariano <marianoh.rodriguez@davinci.edu.ar>
2022-05-28 16:50:02 -0300

    Agregamos DAL y properties con el ConnectionString

---
+++ DAL.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace TP1
+{
+    internal class DAL
+    {
+        private string connectionString;
+        public DAL()
+        {
+            //Cargo la cadena de conexión desde el archivo de properties
+            connectionString = Properties.Resources.ConnectionString;
+        }
+
+        public List<Usuario> inicializarUsuarios()
+        {
+            List<Usuario> misUsuarios = new List<Usuario>();
+
+            //Defino el string con la consulta que quiero realizar
+            string queryString = "SELECT * from dbo.Usuarios";
+
+            // Creo una conexión SQL con un Using, de modo que al finalizar, la conexión se cierra y se liberan recursos
+            using (SqlConnection connection =
+                new SqlConnection(connectionString))
+            {
+                // Defino el comando a enviar al motor SQL con la consulta y la conexión
+                SqlCommand command = new SqlCommand(queryString, connection);
+
+                try
+                {
+                    //Abro la conexión
+                    connection.Open();
+                    //mi objecto DataReader va a obtener los resultados de la consulta, notar que a comando se le pide ExecuteReader()
+                    SqlDataReader reader = command.ExecuteReader();
+                    Usuario aux;
+                    //mientras haya registros/filas en mi DataReader, sigo leyendo
+                    while (reader.Read())
+                    {
+                        aux = new Usuario(reader.GetString(0),reader.GetString(1), reader.GetString(2), reader.GetInt32(3), reader.GetString(4));
+                        misUsuarios.Add(aux);
+                    }
+                    //En este punto ya recorrí todas las filas del resultado de la query
+                    reader.Close();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            return misUsuarios;
+        }
+
+        public int agregarUsuario(int Dni, string Nombre, string Mail, string Password, bool EsADM, bool Bloqueado)
+        {
+            //primero me aseguro que lo pueda agregar a la base
+            int resultadoQuery;
+            int idNuevoUsuario = -1;
+            string connectionString = Properties.Resources.ConnectionString;
+            string queryString = 
+                "INSERT INTO [dbo].[Usuarios] ([Dni],[Nombre],[Apellido],[Mail],[Password],[IntentosFallidos],[Bloqueado],[IdAmigos],[Id]) " +
+                "VALUES (@dni,@nombre,@apellido,@mail,@password,@intentos, @bloqueado, @idAmigo, );";
+            using (SqlConnection connection =
+                new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(queryString, connection);
+                command.Parameters.Add(new SqlParameter("@dni", SqlDbType.Int));
+                command.Parameters.Add(new SqlParameter("@nombre", SqlDbType.NVarChar));
+                command.Parameters.Add(new SqlParameter("@apellido", SqlDbType.NVarChar));
+                command.Parameters.Add(new SqlParameter("@mail", SqlDbType.NVarChar));
+                command.Parameters.Add(new SqlParameter("@password", SqlDbType.NVarChar));
+                command.Parameters.Add(new SqlParameter("@esadm", SqlDbType.Bit));
+                command.Parameters.Add(new SqlParameter("@bloqueado", SqlDbType.Bit));
+                command.Parameters["@dni"].Value = Dni;
+                command.Parameters["@nombre"].Value = Nombre;
+                command.Parameters["@mail"].Value = Mail;
+                command.Parameters["@password"].Value = Password;
+                command.Parameters["@esadm"].Value = EsADM;
+                command.Parameters["@bloqueado"].Value = Bloqueado;
+                try
+                {
+                    connection.Open();
+                    //esta consulta NO espera un resultado para leer, es del tipo NON Query
+                    resultadoQuery = command.ExecuteNonQuery();
+
+                    //*******************************************
+                    //Ahora hago esta query para obtener el ID
+                    string ConsultaID = "SELECT MAX([ID]) FROM [dbo].[Usuarios]";
+                    command = new SqlCommand(ConsultaID, connection);
+                    SqlDataReader reader = command.ExecuteReader();
+                    reader.Read();
+                    idNuevoUsuario = reader.GetInt32(0);
+                    reader.Close();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    return -1;
+                }
+                return idNuevoUsuario;
+            }
+        }
+    }
+}
